44b8a8c9da3e7023c0c20cf18cb41e41946e3e6e
卢子诚 <2193618442@qq.com>
2025-05-29 21:44:30 +0800

    Add files via upload

---
+++ mask_rcnn_r50_fpn_1x_voc.py
@@ -0,0 +1,31 @@
+_base_ = [
+    '../_base_/models/mask-rcnn_r50_fpn.py', # Mask R-CNN 模型配置
+    '../_base_/datasets/voc_coco.py',  # COCO 格式配置
+    '../_base_/schedules/schedule_1x.py', # 导入训练计划
+    '../_base_/default_runtime.py' # 导入运行时配置
+]
+
+# 数据集路径和类别
+data_root = 'D:/py/mmdetection/data/VOC_coco/'
+
+
+# 修改模型输出类别数
+model = dict(
+    roi_head=dict(
+        bbox_head=dict(num_classes=20),
+        mask_head=dict(num_classes=20)
+    )
+)
+
+# 学习率调整
+optim_wrapper = dict(optimizer=dict(lr=0.0025))
+
+# 可视化配置
+vis_backends = [
+    dict(type='LocalVisBackend'),
+    dict(type='TensorboardVisBackend')
+]
+visualizer = dict(
+    type='DetLocalVisualizer', 
+    vis_backends=vis_backends, 
+    name='visualizer')
